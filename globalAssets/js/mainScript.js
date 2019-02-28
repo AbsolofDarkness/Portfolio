9a32b862c6a142cb6e33d853096c3a1a4ed8cf70
AbsolofDarkness <nicpapa98@gmail.com>
2019-02-27 18:23:54 -0600

    added basic randomized quotes in the profile

--- globalAssets/js/mainScript.js
+++ globalAssets/js/mainScript.js
@@ -1,3 +1,13 @@
+function loadQuote () {
+    const quoteBody = $("#quoteText");
+    const quoteAuthor = $("#quoteAuthor");
+
+    let randNum = Math.floor(Math.random() * quotes.length);
+
+    quoteBody.text(quotes[randNum].quote);
+    quoteAuthor.text(quotes[randNum].author);
+}
+
 $('.navbar-collapse ul li a').click(function() {
     /* always close responsive nav after click */
     $('.navbar-toggle:visible').click();
@@ -9,3 +19,5 @@ jQuery(".scroll_to").on("click", function(e){
     $("html, body").stop().animate({scrollTop: new_position.top - 90}, 500);
     e.preventDefault();
 });
+
+window.onload = loadQuote();
