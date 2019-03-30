c2f0567ce0b021bd76c6971907ccdfdb9111d6e6
AbsolofDarkness <nicpapa98@gmail.com>
2019-03-30 11:05:04 -0500

    pushing bamazon

---
+++ assignments/assignment12/queries.sql
@@ -0,0 +1,28 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pear Laptop Computer", "Electronics", 799.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Programming T-Shirt", "Clothing", 25.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bluetooth Speaker", "Electronics", 79.98, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Protein Crisps", "Food", 12.99, 450);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toy Robot", "Kids", 19.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Moon Light", "Furniture", 27.99, 470);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Stripped Sweater", "Clothing", 55.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Power Drill", "Tools", 94.75, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Razor Blades", "Bathroom", 30.49, 75);
