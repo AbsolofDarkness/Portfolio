71a326b6ef094e54c34ee35d730c885068f49c52
AbsolofDarkness <nicpapa98@gmail.com>
2019-04-01 20:11:23 -0500

    added redirects

--- globalAssets/js/mainScript.js
+++ globalAssets/js/mainScript.js
@@ -1,29 +1,31 @@
-function loadQuote () {
-    const quoteBody = $("#quoteText");
-    const quoteAuthor = $("#quoteAuthor");
+function loadQuote() {
+  const quoteBody = $("#quoteText");
+  const quoteAuthor = $("#quoteAuthor");
 
-    let randNum = Math.floor(Math.random() * quotes.length);
+  let randNum = Math.floor(Math.random() * quotes.length);
 
-    quoteBody.text(quotes[randNum].quote);
-    quoteAuthor.text(quotes[randNum].author);
+  quoteBody.text(quotes[randNum].quote);
+  quoteAuthor.text(quotes[randNum].author);
 }
 
-$('.navbar-collapse a').click(function() {
-    /* always close responsive nav after click */
-    $('.navbar-collapse').collapse("hide");
+$(".navbar-collapse a").click(function() {
+  /* always close responsive nav after click */
+  $(".navbar-collapse").collapse("hide");
 });
 
-jQuery(".scroll_to").on("click", function(e){
-    var jump = $(this).attr("href");
-    var new_position = $(jump).offset();
-    $("html, body").stop().animate({scrollTop: new_position.top - 90}, 500);
-    e.preventDefault();
+jQuery(".scroll_to").on("click", function(e) {
+  var jump = $(this).attr("href");
+  var new_position = $(jump).offset();
+  $("html, body")
+    .stop()
+    .animate({scrollTop: new_position.top - 90}, 500);
+  e.preventDefault();
 });
 
 particlesJS.load("particles-js", "./globalAssets/particles.json", () => {
-    console.log("Particles Config Loaded");
-})
+  console.log("Particles Config Loaded");
+});
 
 window.onload = () => {
-    loadQuote();
+  loadQuote();
 };
